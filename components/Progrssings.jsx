eac634a17c72becc016047f437708c2798de8943
Sam-Tech-Ideas <agyemangsamuel235@gmail.com>
2023-06-25 22:37:18 +0000

    updated

--- components/Progrssings.jsx
+++ components/Progrssings.jsx
@@ -105,7 +105,7 @@ const Progressings = () => {
 
   return (
     <>
-      <div className="w-full md:col-span-2 relative lg:h-[70vh] h-[50vh] m-auto p-4 border rounded-lg bg-white">
+      <div className="w-full md:col-span-2 relative lg:h-[70vh] h-[50vh] m-auto p-4 border rounded-lg bg-white md:py-16">
         <p>Total givings</p>
         <h1 className="text-4xl">
           <span className="text-blue-500">
