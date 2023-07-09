80ace895911706b90f143f48bbc70ad5ef70f816
Sam-Tech-Ideas <agyemangsamuel235@gmail.com>
2023-07-09 10:42:45 +0000

    editting

--- pages/events/[id].jsx
+++ pages/events/[id].jsx
@@ -167,7 +167,8 @@ const EventDetail = () => {
                   Starting Date
                 </dt>
                 <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                  {new Date(event.startDate * 1000).toLocaleString()}
+                  {event.startDate &&
+                    new Date(event.startDate.toDate()).toLocaleString()}
                 </dd>
               </div>
               <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
@@ -175,7 +176,8 @@ const EventDetail = () => {
                   End Date
                 </dt>
                 <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-                  {new Date(event.endDate * 1000).toLocaleString()}
+                  {event.endDate &&
+                    new Date(event.endDate.toDate()).toLocaleString()}
                 </dd>
               </div>
               <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
