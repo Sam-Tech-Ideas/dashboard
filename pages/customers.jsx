4c076f7136e3efbb748904f26dd85199881193af
Sam-Tech-Ideas <agyemangsamuel235@gmail.com>
2023-07-19 21:45:13 +0000

    upload

--- pages/customers.jsx
+++ pages/customers.jsx
@@ -1,5 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { collection, getFirestore, getDocs } from "firebase/firestore";
+import {
+  collection,
+  getFirestore,
+  getDocs,
+  updateDoc,
+  doc,
+} from "firebase/firestore";
 import { Card, Typography } from "@material-tailwind/react";
 import Link from "next/link.js";
 import { db } from "@/firebase/config.js";
@@ -8,6 +14,9 @@ const Customers = () => {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
   const [searchQuery, setSearchQuery] = useState("");
+  const [blockAll, setBlockAll] = useState(false);
+  const [sortOption, setSortOption] = useState("all"); // 'all', 'blocked', or 'unblocked'
+  const [sortOrder, setSortOrder] = useState("asc");
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -27,9 +36,68 @@ const Customers = () => {
     fetchUsers();
   }, []);
 
+  const handleSortChange = () => {
+    // Toggle the sorting order when this function is called
+    setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+  };
+
+  const handleSortOptionChange = (e) => {
+    // Update the sorting option when the select value changes
+    setSortOption(e.target.value);
+  };
+
+  const handleBlockAllChange = (e) => {
+    const isChecked = e.target.checked;
+    setBlockAll(isChecked);
+
+    // Update the block field for all users in the state
+    setUsers((prevUsers) =>
+      prevUsers.map((user) => ({
+        ...user,
+        block: isChecked,
+      }))
+    );
+
+    // Update all user documents in the database with the new block status
+    users.forEach((user) => {
+      updateDoc(doc(db, "users", user.id), { block: isChecked });
+    });
+  };
+
+  const handleBlockChange = async (userId, isChecked) => {
+    setUsers((prevUsers) =>
+      prevUsers.map((user) =>
+        user.id === userId ? { ...user, block: isChecked } : user
+      )
+    );
+
+    // Update the user document in the database with the new block status
+    try {
+      await updateDoc(doc(db, "users", userId), { block: isChecked });
+    } catch (error) {
+      console.log("Error updating user:", error);
+    }
+  };
+
   const filteredUsers = users.filter((user) =>
     user.fullName.toLowerCase().includes(searchQuery.toLowerCase())
   );
+
+  const sortedUsers = filteredUsers.slice().sort((a, b) => {
+    // Filter users based on the 'sortOption' state
+    if (sortOption === "blocked" && a.block !== b.block) {
+      return a.block ? -1 : 1;
+    } else if (sortOption === "unblocked" && a.block !== b.block) {
+      return a.block ? 1 : -1;
+    }
+
+    // If 'sortOption' is 'all' or both users have the same 'block' status, sort based on 'sortOrder'
+    if (sortOrder === "asc") {
+      return a.block - b.block;
+    } else {
+      return b.block - a.block;
+    }
+  });
 
   return (
     <div className="">
@@ -37,16 +105,34 @@ const Customers = () => {
         <div>
           <h2 className="text-3xl">All users</h2>
         </div>
-
+        <div className="flex justify-end items-center">
+          <span className="px-2">Sort by block</span>
+          <select
+            className="rounded-md border-gray-500 py-2 px-8 border-2"
+            value={sortOption}
+            onChange={handleSortOptionChange}
+          >
+            <option value="all">All</option>
+            <option value="blocked">Blocked</option>
+            <option value="unblocked">Unblocked</option>
+          </select>
+        </div>
         <div className="flex justify-end  items-center">
           <span className="px-2">Search</span>
-
           <input
             type="text"
             placeholder="Search a user"
-            className="rounded-md border-gray-500 py-2 px-8 border-2"
+            className="rounded-md border-gray-500 py-1 px-8 border-2"
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
+          />
+        </div>
+        <div className="flex justify-end  items-center">
+          <span className="px-2">Block all</span>
+          <input
+            type="checkbox"
+            checked={blockAll}
+            onChange={handleBlockAllChange}
           />
         </div>
       </div>
@@ -55,7 +141,10 @@ const Customers = () => {
         <table className="w-full min-w-max table-auto text-left">
           <thead>
             <tr>
-              <th className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
+              <th
+                className="border-b border-blue-gray-100 bg-blue-gray-50 p-4 cursor-pointer"
+                onClick={handleSortChange}
+              >
                 <Typography
                   variant="small"
                   color="blue-gray"
@@ -88,6 +177,15 @@ const Customers = () => {
                   color="blue-gray"
                   className="font-normal leading-none opacity-70"
                 >
+                  Block action
+                </Typography>
+              </th>
+              <th className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="font-normal leading-none opacity-70"
+                >
                   Action
                 </Typography>
               </th>
@@ -96,12 +194,12 @@ const Customers = () => {
           <tbody>
             {loading ? (
               <tr>
-                <td colSpan="4" className="p-4">
+                <td colSpan="5" className="p-4">
                   Loading...
                 </td>
               </tr>
-            ) : filteredUsers.length > 0 ? (
-              filteredUsers.map((user) => (
+            ) : sortedUsers.length > 0 ? (
+              sortedUsers.map((user) => (
                 <tr key={user.id} className="hover:bg-gray-300">
                   <td className="p-4">
                     <Typography
@@ -131,6 +229,19 @@ const Customers = () => {
                     </Typography>
                   </td>
                   <td className="p-4">
+                    <form>
+                      <input
+                        type="checkbox"
+                        name=""
+                        id=""
+                        checked={user.block || false}
+                        onChange={(e) =>
+                          handleBlockChange(user.id, e.target.checked)
+                        }
+                      />
+                    </form>
+                  </td>
+                  <td className="p-4">
                     <h2 className="text-blue-500">
                       <Link href="/users/[id]" as={`/users/${user.id}`}>
                         View Details
@@ -141,7 +252,7 @@ const Customers = () => {
               ))
             ) : (
               <tr>
-                <td colSpan="4" className="p-4">
+                <td colSpan="5" className="p-4">
                   No users found.
                 </td>
               </tr>
@@ -154,60 +265,3 @@ const Customers = () => {
 };
 
 export default Customers;
-
-{
-  /* <tbody>
-            {loading ? (
-              <tr>
-                <td colSpan="4" className="p-4">
-                  Loading...
-                </td>
-              </tr>
-            ) : users.length > 0 ? (
-              users.map((user, index) => (
-                <tr key={index} className="hover:bg-gray-300">
-                  <td className="p-4">
-                    <Typography
-                      variant="small"
-                      color="blue-gray"
-                      className="font-normal"
-                    >
-                      {user.name}
-                    </Typography>
-                  </td>
-                  <td className="p-4">
-                    <Typography
-                      variant="small"
-                      color="blue-gray"
-                      className="font-normal"
-                    >
-                      {user.job}
-                    </Typography>
-                  </td>
-                  <td className="p-4">
-                    <Typography
-                      variant="small"
-                      color="blue-gray"
-                      className="font-normal"
-                    >
-                      {user.date}
-                    </Typography>
-                  </td>
-                  <td className="p-4">
-                    <h2 className="text-blue-500">
-                      <Link href="/customers/[id]" as={`/customers/${index}`}>
-                        View
-                      </Link>
-                    </h2>
-                  </td>
-                </tr>
-              ))
-            ) : (
-              <tr className="text-center">
-                <td colSpan="4" className="p-4 ">
-                  No users found.
-                </td>
-              </tr>
-            )}
-          </tbody> */
-}
