1a82ea658639fafcb2f3ad9094dfdfcf02bb1ca6
Sam-Tech-Ideas <agyemangsamuel235@gmail.com>
2023-07-22 20:35:49 +0000

    upload

---
+++ components/groups/AddGroup.jsx
@@ -0,0 +1,213 @@
+import React, { Fragment, useState } from "react";
+import {
+  Button,
+  Dialog,
+  DialogHeader,
+  DialogBody,
+  DialogFooter,
+  Input,
+  Progress,
+} from "@material-tailwind/react";
+import { toast } from "react-hot-toast";
+import {
+  Timestamp,
+  collection,
+  doc,
+  serverTimestamp,
+  setDoc,
+} from "firebase/firestore";
+import { db, storage } from "@/firebase/config";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import { nanoid } from "nanoid";
+
+const AddGroup = () => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(!open);
+
+  const [group, setGroup] = useState({
+    id: nanoid(),
+    name: "",
+    description: "",
+    groupImage: "",
+
+    groupLeader: "",
+    meetingDays:[""],
+    members: [""],
+
+  });
+
+  const [uploadProgress, setUploadProgress] = useState(0);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setGroup({ ...Group, [name]: value });
+  };
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    console.log(file);
+
+    const storageRef = ref(storage, `Grimages/${Date.now()}${file.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setUploadProgress(progress);
+      },
+      (error) => {
+        alert(error.message);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setGroup({ ...Group, groupImage: downloadURL });
+          alert("Image uploaded successfully");
+        });
+      }
+    );
+  };
+
+  const handleAddGroup = async (e) => {
+    //e.prGroupDefault();
+    console.log(Group);
+
+    try {
+      const docRef = doc(db, "groups", group.id); // Replace `group.id` with the actual document ID
+
+ 
+
+      const groupData = {
+        id: group.id,
+        name: group.name,
+
+        description: group.description,
+        groupImage: group.groupImage,
+        groupLeader: group.groupLeader,
+        meetingDays: group.meetingDays,
+        members: group.members,
+
+
+
+
+      
+      };
+
+      await setDoc(docRef, groupData);
+      toast.success("Group created successfully");
+
+      handleOpen();
+
+      //console.log("Document written with ID: ", docRef.id);
+    } catch (error) {
+      console.log(error);
+      alert(error.message);
+    }
+  };
+
+  return (
+    <div>
+      <Fragment>
+        <div className="flex justify-between px-4 pt-8">
+          <div className="flex items-center">
+            <button
+              className="bg-blue-600 text-white px-4 py-2 rounded-lg "
+              onClick={handleOpen}
+            >
+              Create Group
+            </button>
+          </div>
+        </div>
+
+        <Dialog
+          open={open}
+          handler={handleOpen}
+          animate={{
+            mount: { scale: 1, y: 0 },
+            unmount: { scale: 0.9, y: -100 },
+          }}
+        >
+          <DialogHeader>Create Group</DialogHeader>
+          <DialogBody divider>
+            <form onSubmit={handleAddGroup}>
+              <div className="m-2">
+                <label htmlFor="">Group name</label>
+                <input
+                  type="text"
+                  placeholder="Group name"
+                  className="border-2 border-gray-300 p-2 rounded-lg w-full"
+                  onChange={handleInputChange}
+                  value={group.name}
+                  name="name"
+                />
+              </div>
+              <div className="m-2">
+                <label htmlFor="">group description</label>
+                <input
+                  type="text"
+                  placeholder="group description"
+                  className="border-2 border-gray-300 p-2 rounded-lg w-full"
+                  onChange={handleInputChange}
+                  value={group.description}
+                  name="description"
+                />
+              </div>
+             
+              {/* <div className="m-2">
+                <label htmlFor="">Event date(end date)</label>
+                <input
+                  type="date"
+                  placeholder="End date"
+                  className="border-2 border-gray-300 p-2 rounded-lg w-full "
+                  onChange={handleInputChange}
+                  value={event.endDate}
+                  name="endDate"
+                />
+              </div> */}
+
+              {uploadProgress === 0 ? null : (
+                <Progress value={uploadProgress} color="blue" />
+              )}
+
+              <div className="flex flex-col mt-2">
+                <label htmlFor="imageUrl">Upload Image</label>
+                <input
+                  type="file"
+                  accept="image/*"
+                  name="imageUrl"
+                  className="border border-gray-300 p-2 rounded-lg focus:outline-none focus:border-blue-400"
+                  onChange={(e) => handleImageChange(e)}
+                />
+                {group.groupImage === "" ? null : (
+                  <input
+                    className="border border-gray-300 p-2 rounded-lg focus:outline-none focus:border-blue-400"
+                    type="text"
+                    name="imageUrl"
+                    placeholder="Group image"
+                    value={group.groupImage}
+                    disabled
+                  />
+                )}
+              </div>
+
+
+             
+
+              <div className="flex justify-between m-4">
+                <button
+                  className="bg-blue-400 text-white px-6 py-2 rounded-xl"
+                  type="submit"
+                >
+                  <span>Upload</span>
+                </button>
+              </div>
+            </form>
+          </DialogBody>
+        </Dialog>
+      </Fragment>
+    </div>
+  );
+};
+
+export default AddGroup;
--- pages/Groups.jsx
+++ pages/Groups.jsx
@@ -9,6 +9,7 @@ import {
   Avatar,
 } from "@material-tailwind/react";
 import Link from "next/link";
+import AddGroup from "@/components/groups/AddGroup";
 
 const Groups = () => {
   const [groups, setGroups] = useState([]);
@@ -34,9 +35,12 @@ const Groups = () => {
               </h1>
             </div>
             <div>
-              <button className="bg-blue-500 px-6 py-2 text-white rounded">
+              {/* <button className="bg-blue-500 px-6 py-2 text-white rounded">
                 Create Group
-              </button>
+              </button> */}
+              <div className="pb-8">
+                <AddGroup />
+              </div>
             </div>
           </div>
 
@@ -44,42 +48,37 @@ const Groups = () => {
             {groups.map((group) => (
               <div key={group.id} className="xl:w-1/4 md:w-1/2 p-4">
                 <Link href={`/groups/${group.id}`}>
-                <Card
-                  shadow={false}
-                  className="relative grid h-[20rem] w-full max-w-[48rem] items-end justify-center overflow-hidden text-center"
-                >
-                  <CardHeader
-                    floated={false}
+                  <Card
                     shadow={false}
-                    color="transparent"
-                    className='absolute inset-0 m-0 h-full w-full rounded-none'
-                    style={
-
-                        {
-                            backgroundImage: `url(${group.groupImage})`,
-                            backgroundSize: "cover",
-                            backgroundPosition: "center",
-                            backgroundRepeat: "no-repeat",
-
-                        }
-                    }
-                   
+                    className="relative grid h-[20rem] w-full max-w-[48rem] items-end justify-center overflow-hidden text-center"
                   >
-                    <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-t from-black/80 via-black/50" />
-                  </CardHeader>
-                  <CardBody className="relative py-14 px-6 md:px-12">
-                    <Typography
-                      variant="h2"
-                      color="white"
-                      className="mb-6 font-medium leading-[1.5]"
+                    <CardHeader
+                      floated={false}
+                      shadow={false}
+                      color="transparent"
+                      className="absolute inset-0 m-0 h-full w-full rounded-none"
+                      style={{
+                        backgroundImage: `url(${group.groupImage})`,
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                        backgroundRepeat: "no-repeat",
+                      }}
                     >
-                      {group.name}
-                    </Typography>
-                    <Typography variant="h5" className="mb-4 text-gray-400">
-                      {group.members.length} Members
-                    </Typography>
-                  </CardBody>
-                </Card>
+                      <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-t from-black/80 via-black/50" />
+                    </CardHeader>
+                    <CardBody className="relative py-14 px-6 md:px-12">
+                      <Typography
+                        variant="h2"
+                        color="white"
+                        className="mb-6 font-medium leading-[1.5]"
+                      >
+                        {group.name}
+                      </Typography>
+                      <Typography variant="h5" className="mb-4 text-gray-400">
+                        {group.members.length} Members
+                      </Typography>
+                    </CardBody>
+                  </Card>
                 </Link>
               </div>
             ))}
